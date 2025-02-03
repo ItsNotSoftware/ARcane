f93ef9564166b81e9358caf9f217c174048b2ee0
Diogo Ferreira <itsnotsoftware@gmail.com>
2025-02-03 21:11:32 +0000

    Logger first steps

--- include/ARcane.hpp
+++ include/ARcane.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
-// For use by ARcane applications
 #include "ARcane/Application.hpp"
+#include "ARcane/Log.hpp"
+
+// ---Entry Point---
 #include "ARcane/EntryPoint.hpp"
+// ------------------
--- include/ARcane/EntryPoint.hpp
+++ include/ARcane/EntryPoint.hpp
@@ -3,6 +3,9 @@
 extern ARcane::Application* ARcane::CreateApplication();
 
 int main(int argc, char** argv) {
+    ARcane::Log::Init();
+    ARcane::Log::GetCoreLogger()->info("Initialized Log!");
+
     auto app = ARcane::CreateApplication();
     app->Run();
     delete app;
