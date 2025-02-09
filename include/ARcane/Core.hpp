fb51a49fc9ee15a58a2bdb10c50640b1f1767447
Diogo Ferreira <itsnotsoftware@gmail.com>
2025-02-09 15:45:47 +0000

    Basic event system

--- include/ARcane/Core.hpp
+++ include/ARcane/Core.hpp
@@ -4,8 +4,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include <functional>
 #include <cstdint>
 #include <exception>
 
 #include "ARcane/Log.hpp"
+
+#define BIT(x) (1 << x)
