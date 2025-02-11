083abbfe6443f2cdf9cb66208c4323825d3c20ba
Diogo Ferreira <itsnotsoftware@gmail.com>
2025-02-11 15:36:19 +0000

    Input pooling

--- include/ARcane.hpp
+++ include/ARcane.hpp
@@ -4,13 +4,8 @@
 #include "ARcane/Input.hpp"
 #include "ARcane/Layer.hpp"
 #include "ARcane/Log.hpp"
-<<<<<<< Updated upstream
-=======
-
-#include "ARcane/ImGuiLayer.hpp"
-    >>>>>>> Stashed changes
 #include "ARcane/ImGuiLayer.hpp"
 
 // ---Entry Point---
 #include "ARcane/EntryPoint.hpp"
-    // ------------------
+// ------------------
